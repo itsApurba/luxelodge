4b5449130b0c1f234056fa69d9bb5638932d3223
Apurba Adhikary <apurba.apurbaadhikary@gmail.com>
2023-04-29 00:17:53 +0000

    ✨Added: Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import './globals.css'
-import {Nunito} from "next/font/google"
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
 export const metadata = {
-  title: 'Luxelodge',
-  description: 'Book your coords',
-}
+  title: "Luxelodge",
+  description: "Book your coords",
+};
 
 const font = Nunito({
-  subsets:["latin"]
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
+    <html lang='en'>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
